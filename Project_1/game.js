23bb28cccde135d88d9a1fe1731f933c7f8da0f5
Melaina Valentine <emme313@Melainas-MacBook-Pro.local>
2019-09-15 10:48:06 -0700

    added housekeeping markdown file

---
+++ Project_1/game.js
@@ -0,0 +1,13 @@
+// Display start up page with Title, Image of the GGs, instructions, and button to play the game
+    // Use a timed modal
+
+// Set up question pages
+    // Include a space for the score
+    // Keep track of which question they're on
+
+// Set up a scoring page
+    // Display user's total score
+    // Display matching GG and witty comment on how they relate
+    // Include two buttons
+        // One button to exit the game which takes you back to the start up page
+        // Another button to the questions
