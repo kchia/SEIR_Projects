fa0e84aa6a435702d17bcd1a4a35a73413018d7f
Melaina Valentine <emme313@Melainas-MacBook-Pro.local>
2019-09-15 21:41:46 -0700

    Updated tab names, and added new HTML and JS files for trivia Qs.

--- Project_1/app.js
+++ Project_1/app.js
@@ -18,15 +18,3 @@ const closeModal = () => {
 // Add event listeners to buttons to run above functions
 openDirections.addEventListener("click", openModal);
 closeDirections.addEventListener("click", closeModal);
-
-
-// Set up question pages
-    // Include a space for the score
-    // Keep track of which question they're on
-
-// Set up a scoring page
-    // Display user's total score
-    // Display matching GG and witty comment on how they relate
-    // Include two buttons
-        // One button to exit the game which takes you back to the start up page
-        // Another button to the questions
---
+++ Project_1/questions.js
@@ -0,0 +1,10 @@
+// Set up question pages
+    // Include a space for the score
+    // Keep track of which question they're on
+    console.log("This works");
+    // Set up a scoring page
+        // Display user's total score
+        // Display matching GG and witty comment on how they relate
+        // Include two buttons
+            // One button to exit the game which takes you back to the start up page
+            // Another button to the questions
