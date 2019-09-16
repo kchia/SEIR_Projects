f7c7f06f8b915e14be188a0fa89e59a51cd4076f
Melaina Valentine <emme313@Melainas-MacBook-Pro.local>
2019-09-15 18:47:18 -0700

    updates to HTML, CSS, and JS.

--- Project_1/game.js
+++ Project_1/game.js
@@ -1,5 +1,24 @@
 // Display start up page with Title, Image of the GGs, instructions, and button to play the game
-    // Use a timed modal
+    // Use a timed modal for the directions with an exit button
+
+    // Establish necessary variables to allow modal to appear and disappear on demand
+const openDirections = document.getElementById("directions");
+const modal = document.getElementById("modal");
+const closeDirections = document.getElementById("close");
+
+// Establish functions to open and close modal
+const openModal = () => {
+    modal.style.display = "block";
+};
+
+const closeModal = () => {
+    modal.style.display = "none";
+}
+
+// Add event listeners to buttons to run above functions
+openDirections.addEventListener("click", openModal);
+closeDirections.addEventListener("click", closeModal);
+
 
 // Set up question pages
     // Include a space for the score
